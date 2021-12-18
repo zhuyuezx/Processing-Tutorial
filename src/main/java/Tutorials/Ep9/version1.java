a5efc6e2d342934cb785b9da69c5273f1bd3a323
Yuezhexuan Zhu <zhuyuezx@gmail.com>
2021-12-18 01:10:34 -0500

    Ep9 update

---
+++ src/main/java/Tutorials/Ep9/version1.java
@@ -0,0 +1,93 @@
+package Tutorials.Ep9;
+
+import processing.core.PApplet;
+
+public class version1 extends PApplet {
+
+    int grid = 400;
+    float angle;
+    int mx = 80, my = 135;
+    float scaleVar = 1;
+    int resetCounter = 1;
+    boolean initialize = false;
+
+    public void settings() {
+        size(1920, 1080, P3D);
+    }
+
+    public void draw() {
+        if (!initialize) {
+            rectMode(CENTER);
+            fill(255, 0, 0);
+            noStroke();
+
+            mx = (width - floor((float) width / (grid * 2)) * grid * 2) / 2;
+            //my = (height - floor((float) width / (grid * 2)) * grid * 2) / 2;
+            my = (height - floor((float) height / (grid * 2)) * grid * 2) / 2;
+
+            initialize = true;
+        }
+        background(15, 20, 30);
+
+        translate((float) width / 2, (float) height / 2);
+//        scaleVar = map(mouseX, 0, width, (float) 0.1, 5);
+        scaleVar = lerp(scaleVar, map(angle, 0, 180,
+                (float) (resetCounter - 0.7), resetCounter), (float)0.1);
+        scale(scaleVar);
+        for (int i = mx + grid - width / 2; i < width / 2 - mx; i += grid * 2) {
+            for (int j = my + grid - height / 2; j < height / 2 - my; j += grid * 2) {
+                pushMatrix();
+                translate(i, j);
+
+                // top left
+                pushMatrix();
+                translate((float) -grid / 2, (float) -grid / 2);
+                rotateX(radians(angle));
+                rotateY(-radians(angle));
+                rect(0, 0, grid, grid);
+                popMatrix();
+
+                // top right
+                pushMatrix();
+                translate((float) grid / 2, (float) -grid / 2);
+                rotateY(radians(angle));
+                rotateX(-radians(angle));
+                rect(0, 0, grid, grid);
+                popMatrix();
+
+                // bottom left
+                pushMatrix();
+                translate((float) -grid / 2, (float) grid / 2);
+                rotateY(radians(angle));
+                rotateX(radians(angle));
+                rect(0, 0, grid, grid);
+                popMatrix();
+
+                // bottom right
+                pushMatrix();
+                translate((float) grid / 2, (float) grid / 2);
+                rotateX(-radians(angle));
+                rotateY(-radians(angle));
+                rect(0, 0, grid, grid);
+                popMatrix();
+
+                popMatrix();
+            }
+        }
+        angle++;
+
+        if (angle >= 180) {
+            grid /= 2;
+            if (grid <= 10) {
+                grid = 400;
+                resetCounter = 0;
+            }
+            angle = 0;
+            resetCounter += 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Tutorials.Ep9.version1");
+    }
+}
